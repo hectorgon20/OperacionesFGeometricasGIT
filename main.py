cf1cf92f11fe572af14db163109e1367fbcd176e
hectorgon20 <hectorgon_20@outlook.com>
2023-06-01 20:58:10 +0200

    Creación del modulo calcular volumen de Figuras Geometricas

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 
 import math
 from CalcularAreas import *
+from CalcularVolumen import *
+
 
 if __name__ == '__main__':
     print(__name__)
+
     lado = int(input("Introduzca lado: "))
     print("Área del cuadrado: ", area_cuadrado(lado))
+
     radio = int(input("Introduzca el radio del círculo: "))
     print("Área del círculo: ", area_circulo(radio))
+
+    print("Volumen de la esfera: ", volumen_esfera(radio))
+
+    print("Área del Rombo: ", area_rombo(25, 50)
+
